f37f3c8a233e109403aeb3fdd89ab81170006c72
Pokisreal <pokpong.smartboy@gmail.com>
2024-07-19 16:13:43 +0700

    เปรียบเทียบค่า

--- ep1/index.ts
+++ ep1/index.ts
@@ -6,3 +6,10 @@ let อยากกลับบ้าน:boolean = false
 console.log(firstname)
 console.log(y)
 console.log(อยากกลับบ้าน)
+
+let a: number = 210
+let b: number = 130
+
+console.log(a === b)
+console.log(a > b) 
+console.log(a <= b)
