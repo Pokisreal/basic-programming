da593074f6a1e7a7bab980988cfc47ee351cc74b
tanapat058 <pokpong.smartboy@gmail.com>
2024-07-23 14:42:57 +0700

    save

---
+++ ep.2/index.ts
@@ -0,0 +1,14 @@
+function add(a: number, b: number) {
+    return a + b 
+}
+console.log(add(5, 8))
+
+function minus(a: number, b: number) {
+    return a - b 
+}
+console.log(minus(6, 6))
+
+function multiply(a: number, b: number, c: number, d:number) {
+    return a * b * c * d
+}
+console.log(multiply(8, 9, 1, 5))
