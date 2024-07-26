e90c22887c4910bcd5364cd7a87c501ac32d4d7d
pittinun <pokpong.smartboy@gmail.com>
2024-07-26 16:12:51 +0700

    save2

--- ep.3/index.ts
+++ ep.3/index.ts
@@ -38,5 +38,37 @@ function readi(stname: string, nd: string, rd: string) {
     }
 }
 
-readi('mix' , 'zane' , 'pok')
+readi('mix' , 'XD' , 'pok')
 
+
+function ready(firstname: string, secname: string, thirdname: string) {
+    if (!(firstname === 'mix' || secname === 'zane') && thirdname === 'forth') {
+        console.log('พร้อมเรียน') 
+    } else {
+        console.log('ยังไม่ร้อม')
+    }
+}
+
+ready('mix' , 'zane' , 'pok')
+
+
+function military(gender: string, tall: number, weight: number) {
+    if (gender === 'man' && (tall > 170 || (weight > 50 && weight <= 110))) {
+        console.log('จับใบดำใบเเดง') 
+    } else {
+        console.log('ไม่เข้าเกณฑ์')
+    }
+}
+    
+
+military('man', 160 , 115)
+
+
+function digitalwall(age: number, money: number, moneyback: number) {
+    if (age >= 16 && money < 70000 && moneyback < 500000) {
+        console.log("รับ 1000 บาท")
+    } else {
+        console.log("อด...haha")
+    }
+}
+digitalwall(20,150000000,50000000000)
