0b33c49a455f5a1dd0a12a5b7f5c0193a1176580
pittinun <pokpong.smartboy@gmail.com>
2024-08-02 15:20:26 +0700

    save

---
+++ ep.4/index.ts
@@ -0,0 +1,97 @@
+// function sayHi(dayOfweek:number){
+//     if(dayOfweek === 1) {
+//         console.log('สวัสดีวันจันทร์') 
+//         }else if (dayOfweek === 2) {
+//             console.log('สวัสดีวันอังคาร')
+            
+//         }else if (dayOfweek === 3) {
+//             console.log('สวัสดีวันพุธ')
+            
+//         }else if (dayOfweek === 4) {
+//             console.log('สวัสดีวันพฤหัสบดี')
+            
+//         }else if (dayOfweek === 5) {
+//             console.log('สวัสดีวันศุกร์')
+            
+//         }else if (dayOfweek === 6) {
+//             console.log('สวัสดีวันเสาร์')
+            
+//         }else if (dayOfweek === 7) {
+//             console.log('สวัสดีวันอาทิตย์')
+            
+//         }
+            
+//         }
+
+// sayHi(6)
+
+
+// function sayhey(dayOfweek:number) {
+//     switch (dayOfweek) {
+//         case 1:
+//         console.log('สวัสดีวันอาทิตย์')
+//         break
+//         case 2:
+//         console.log('สวัสดีวันอังคาร')
+//         break
+//         case 3:
+//         console.log('สวัสดีวันพุธ')
+//         break
+//         case 4:
+//         console.log('สวัสดีวันพฤหัสบดี')
+//         break
+//         case 5:
+//         console.log('สวัสดีวันศุกร์')
+//         break
+//         case 6:
+//         console.log('สวัสดีวันเสาร์')
+//         break
+//         case 7:
+//         console.log('สวัสดีวันจันทร์')
+//         break
+//         default:
+//         console.log('ใส่วันมาผิด')
+//     }
+// }
+// sayhey(5)
+
+// function cloth(size:string) {
+//     switch(size) {
+//         case "s":
+//         console.log('ผอม')
+//         break
+//         case "m":
+//         console.log('ผอม')
+//         break
+//         case "l":
+//         console.log('ท้วม')
+//         break
+//         case "xl":
+//         console.log('อ้วน')
+//         break
+//         case "2xl":
+//         console.log('อ้วน')
+//         break
+//         default:
+//         console.log('0-0')
+
+//     }
+// }
+// cloth("l")
+
+function fixedDecimal(n:number, digit: number) {
+    const x = n.toFixed(digit)
+    return x
+}
+
+console.log(fixedDecimal(1000,3))
+
+function fixedDecimal1(n:number){
+    const g = n.toLocaleString()
+    return g
+}
+
+console.log(fixedDecimal1(1000))
+
+
+const str: string = "I can't go "
