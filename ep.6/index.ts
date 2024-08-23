8a07020e63b1c2c4c64eef5a602e9ffc3110cf22
pittinun <pokpong.smartboy@gmail.com>
2024-08-23 15:55:56 +0700

    save1

---
+++ ep.6/index.ts
@@ -0,0 +1,130 @@
+// let car1: string = 'Honda'
+// let car2: string = 'yamaha'
+// let car3: string = 'toyota'
+// let car4: string = 'Mazda'
+// let car5: string = 'Suzuki'
+
+// for(let i = 1; i <=10; i++) {
+//     console.log(i)
+// }
+
+// let sum: number = 0
+
+// for (let i = 1; i <= 365; i++) {
+//     sum = sum + i
+// }
+// console.log('Total: ', sum)
+
+// let sum:number = 0
+//     let i: number = 1
+//         while (i <= 31) {
+//             sum = sum + i
+//             i++
+
+// }
+
+// console.log('Total: ', sum)
+
+// let sum: number = 0
+// let i: number = 1 
+
+// do {
+//     sum = sum + i
+//     i++
+// } while (i <= 31)
+
+// console.log('Total: ', sum)
+
+// let count: number = 0
+
+// while (true) {
+//     console.log("โสดเว้ย")
+//     count++
+//     if (count === 10) {
+//         break
+//     }
+// }
+
+// let age: number = 1
+// while (true) {
+//     console.log('ฉันอายุ', age , 'ปี')
+//     age++
+//     if (age === 18) {
+//         break
+//     }
+// }
+
+// for (let i = 1; i <= 13; i++) {
+//     console.log('ฉันอายุ ' + i + 'ปี')
+// }
+
+// let i: number = 1
+// while (i <= 13) {
+//     console.log('ฉันอายุ' + i + 'ปี')
+//     i++
+// }
+
+// let i: number = 1
+// do {
+//     console.log('ฉันอายุ' + i + 'ปี')
+//     i++
+// } while(i <= 13)
+
+    // let car1: string = 'toyota'
+// let car2: string = 'Honda'
+// let car3: string = 'Suzuki'
+// let car4: string = 'yamaha'
+// let car5: string = 'mazda'
+
+// const cars: string[] = ['toyota', 'Honda', 'Suzuki' , 'yamaha', 'mazda']
+// const num:number[] = [1,2,3,4,5]
+// const bool: boolean[] = [true,true,false,false]
+
+// const cars: string[] = ['toyota', 'Honda', 'Suzuki' , 'yamaha', 'mazda']
+// const str: string = 'ชอบก็จัด ประหยัดทำไม'
+
+// console.log(str.charAt(2))
+
+// const cars: string[] = ['toyota', 'Honda', 'Suzuki' , 'yamaha', 'mazda']
+
+// cars[2] = 'Isuzu'
+
+// console.log(cars[2])
+
+// const cars: string[] = ['toyota', 'Honda', 'Suzuki' , 'yamaha', 'mazda', 'BYD']
+
+// for( let i = 0; i< cars.length; i++) {
+//     console.log(cars[i])
+// }
+
+// const cars: string[] = ['toyota', 'Honda', 'Suzuki' , 'yamaha', 'mazda']
+
+// for(let i = 0; i< cars.length; i++) {
+//     if(cars[i] === 'yamaha') {
+//     console.log('รถซื้อเเกง จะเเรงได้ไง')
+//     }
+// }
+
+// let i: number = 0
+
+// while(i< cars.length) {
+//     if (cars[i] === 'yamaha') {
+//         console.log('รถซื้อเเกง จะเเรงได้ไง')
+//     }
+// }
+
+// console.log(cars.includes('BYD'))
+
+// const str: string = 'Forth'
+
+// console.log(str.indexOf('x'))
+// console.log(cars.include('honda'))
+
+// const fname: string[] =['kew','ruj','oam','pete','sorn'] 
+
+// for(let i = 0; i< fname.length; i++) {
+//     if(fname[i] === 'kew') {
+//     console.log('ไปกินหมูกระทะกัน')
+//     }
+// }
+
