cccdf3668cb2dc752ac20cbba0ec1ed7a4541760
Pokisreal <pokpong.smartboy@gmail.com>
2024-09-06 00:45:54 +0700

    save1

---
+++ HomeWork/index.ts
@@ -0,0 +1,15 @@
+const me = {
+    firstName: 'Pi',
+    lastName: 'Ha',
+    age: 15 ,
+    telephone: '911',
+    birthday: 121/55/2666,
+    address: 'eiei',
+    gender: 'male',
+    zipcode: '9199218',
+    road: 'aw man',
+    email: 'rukiddingme4@gmail.com',
+    district: 'idkman',
+    country: 'mars'
+    
+}
