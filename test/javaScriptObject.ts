de899c2fba115a17f2a35b7f49acd29666a9f59a
Pokisreal <pokpong.smartboy@gmail.com>
2024-10-04 15:00:53 +0700

    save2

--- test/javaScriptObject.ts
+++ test/javaScriptObject.ts
@@ -13,3 +13,37 @@ const person = {
 }
 
 person.sister.lastName = 'Smith'
+
+
+const number = [1, 2, 3, 4, 5] // -> ['1','2','3','4','5']
+// เเบบยาก
+function covertToArrayString(array: number[]) {
+    const result: string[] = [];
+
+    for (let i = 0; i < array.length; i++) {
+        result.push(array[i].toString())
+    } 
+    return result
+}
+
+//เเบบง่าย ใช้ .map
+
+function covertToArrayStringWithMap(array:number[]) {
+    return array.map(function(element) {
+        return element.toString
+    })
+}
+ 
+const names = ["John Doe", "Jane Smith", "Jack Brown"];
+
+function cutNameWithMap(array: string[]) {
+    return array.map(function(element, index){
+        const cutName = element.split(" ")
+        return {
+            firstName: cutName[0],
+            lastName: cutName[1]
+        }
+    })
+}
+
+console.log(cutNameWithMap(names))
